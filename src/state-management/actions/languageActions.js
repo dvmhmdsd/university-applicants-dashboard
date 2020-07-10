93c514ee69033fa10b7067e974a08b9661c53510
mohamedsaad <mahameds19999@gmail.com>
2020-07-10 22:40:17 +0200

    Make language reducer

---
+++ src/state-management/actions/languageActions.js
@@ -0,0 +1,19 @@
+import { GET_LANGUAGE, SET_LANGUAGE } from "state-management/constants";
+import detectBrowserLanguage from "detect-browser-language";
+
+export const getLanguageKey = () => (dispatch) => {
+  let languageKey =
+    JSON.parse(localStorage.getItem("lang")) || detectBrowserLanguage() || "en";
+
+  dispatch({
+    type: GET_LANGUAGE,
+    payload: languageKey,
+  });
+};
+
+export const setLanguageKey = (key) => (dispatch) => {
+  dispatch({
+    type: SET_LANGUAGE,
+    payload: key,
+  });
+};
