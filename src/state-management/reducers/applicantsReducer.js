8a29752c3be24df68847340a42c580ed1035b480
mohamedsaad <mahameds19999@gmail.com>
2020-07-11 01:34:36 +0200

    Change students to applicants

--- src/state-management/reducers/applicantsReducer.js
+++ src/state-management/reducers/applicantsReducer.js
@@ -1,4 +1,4 @@
-import { GET_STUDENTS } from "state-management/constants";
+import { GET_APPLICANTS } from "state-management/constants";
 
 let initialState = {
   list: [],
@@ -6,7 +6,7 @@ let initialState = {
 };
 
 export default (state = initialState, action) => {
-  return action.type === GET_STUDENTS
+  return action.type === GET_APPLICANTS
     ? {
         ...state,
         list: action.payload,
--- src/state-management/reducers/index.js
+++ src/state-management/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import modeReducer from "./modeReducer";
 import languageReducer from "./languageReducer";
-import studentsReducer from "./studentsReducer";
+import applicantsReducer from "./applicantsReducer";
 
 export default combineReducers({
-  students: studentsReducer,
+  students: applicantsReducer,
   mode: modeReducer,
   lang: languageReducer,
 });
